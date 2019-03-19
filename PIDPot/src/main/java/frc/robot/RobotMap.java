c27d6728eadff41b5c9f09e8b95908ad544f9c1f
SetonRoboticsMFoor <48261500+SetonRoboticsMFoor@users.noreply.github.com>
2019-03-19 11:06:02 -0700

    Working PID code
    This is working code for PID with a potentiometer. Adjust the setpoints in the motor forward/ reverse commands and tune the PID values in the motor subsystem.

---
+++ PIDPot/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+public class RobotMap {
+
+  // Create the constant for the motor channel
+  public static int MOTOR_CH = 8;
+
+  // Create the constant for the herder arm pot
+  public static int HERDER_ARM_POT_CH = 0;
+
+  // Create the constant for the joystick channel
+  public static int STICK_CH = 0;
+
+  // Create the constants for the buttons used on the joystick
+  public static int FORWARD_BUTTON_CH = 4;
+  public static int REVERSE_BUTTON_CH = 2;
+}
